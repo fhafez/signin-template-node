5bf661f19fb0313afb0c12700c073abb21cc7931
fhafez <fadi.hafez@gmail.com>
2020-02-23 13:08:34 -0500

    moved POST code out of getProvider

--- gcp-functions/getProviders/index.js
+++ gcp-functions/getProviders/index.js
@@ -25,68 +25,6 @@ exports.getProviders = (req, res) => {
 
       } else {
 
-        // create a new entry
-        if (req.body.description) {
-
-          let desc = req.body.description || '';
-          let providerID = req.body.providerID || '';
-        
-          // if the providerID was not provided in the body of a POST, create the provider
-          if (providerID == '') {
-            let query = datastore.createQuery('Provider'); //.select('__key__');
-            if (desc) {
-              query.filter('description','=',desc.trim());
-            }
-
-            // look for duplicate provider descriptions
-            datastore.runQuery(query).then(results => {
-              let count = results[0].length;
-              const providers = results[0];
-              
-              var keys = [];
-              providers.forEach(provider  => {
-                const providerKey = provider[datastore.KEY];
-                keys.push(providerKey);
-                console.log(provider);
-              });
-              
-              console.log(keys);
-
-              if (count == 0) {
-                var dataToSave = {
-                  description: desc.trim()
-                }
-
-                const key = providerID > 0 ? datastore.key({path: ['Provider', datastore.int(providerID)]}) : datastore.key(kindName);
-                datastore
-                    .save({
-                        key: key,
-                        data: dataToSave,
-                    })
-                    .then(data => {
-  //                    dataToSave.id = key;
-                      res.send(data[0]["mutationResults"][0]["key"]["path"][0]);                     
-                	  })
-                    .catch(err => {
-                        console.error('ERROR:', err);
-                        res.status(400).send(err);
-                        return;
-                    });
-
-              } else {
-                
-                console.log("printing keys now");
-                var keys = results.map(function(provider) {
-                  console.log(provider[datastore.KEY]);
-                });
-              res.status(401).send("{error: 'duplicate provider found'}");      
-              }
-            })
-            .catch(err => { console.error('ERROR:', err); });
-          }
-
-        } else {
-
           // if provider description was provided in the querystring of a GET then return the provider record
           let desc = req.query.description || '';
 
@@ -120,8 +58,6 @@ exports.getProviders = (req, res) => {
           })
           .catch(err => { console.error('ERROR:', err); });
         }
-
-      }
 
     });
 };
