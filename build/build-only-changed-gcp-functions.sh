8ce16485d7bbf43f88e8eb1e85648b8d00f0c0ed
fhafez <fadi.hafez@gmail.com>
2020-02-26 22:59:01 -0500

    fixed build script

--- build/build-only-changed-gcp-functions.sh
+++ build/build-only-changed-gcp-functions.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 ALL_DIRS=`ls -1 ../gcp-functions`
-CHANGED_FILES=`git diff HEAD^ HEAD --name-only`
+#CHANGED_FILES=`git diff HEAD^ HEAD --name-only`
+CHANGED_FILES=`git diff master origin/master --name-only`
 
 #echo $CHANGED_FILES
 #echo $ALL_DIRS
 
-for f in ${CHANGED_FILES}; do   
-   echo $f
-   if [[ $ALL_DIRS == *$f* ]]; then 
-      echo "yes"; 
+for f in ${CHANGED_FILES}; do
+   fname=`echo $f | awk -F/ '{print $2}'`
+   # echo $f
+   if [[ $ALL_DIRS == *"$fname"* ]]; then 
+      echo $fname; 
+      gcloud --quiet beta functions deploy $fname --source https://source.developers.google.com/projects/scenic-setup-231121/repos/github_fhafez_signin-template-node/moveable-aliases/master/paths/gcp-functions/$fname --trigger-http &
    fi;
 done
 
-#gcloud --quiet beta functions deploy function-1 --source https://source.developers.google.com/projects/scenic-setup-231121/repos/github_fhafez_signin-template-node/moveable-aliases/master/paths/gcp-functions/function-1 --trigger-http
 
