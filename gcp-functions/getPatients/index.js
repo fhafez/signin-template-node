8c0d61712aaa1547bfadba7f9796a9f3147e1792
fhafez <fadi.hafez@gmail.com>
2020-02-26 23:28:29 -0500

    11:28 commit

--- gcp-functions/getPatients/index.js
+++ gcp-functions/getPatients/index.js
@@ -9,7 +9,7 @@ const {Storage} = require('@google-cloud/storage');
 const storage = new Storage();
 const BUCKET_NAME = "parcontario-scar-signatures";
 
-// just testing ci/cd - take 3
+// just testing ci/cd - take 4
 //
 const kindName = 'Patient';
 
