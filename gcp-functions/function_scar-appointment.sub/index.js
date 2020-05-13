4377fa7e396964dda24a3bbad8345118c6c95288
fhafez <fadi.hafez@gmail.com>
2020-05-13 01:04:39 -0400

    gcp function testing

--- gcp-functions/function_scar-appointment.sub/index.js
+++ gcp-functions/function_scar-appointment.sub/index.js
@@ -7,5 +7,6 @@ exports.printAppointment = async (data, context) => {
     const msg = data;
     const details = msg.data ? Buffer.from(msg.data, 'base64').toString() : '';
 
+    // just print the message received
     console.log(`Got a message! ${details}`);
 };
