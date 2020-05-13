d7d84b749314053243896d6c564809a5a46a1a55
fhafez <fadi.hafez@gmail.com>
2020-05-13 00:38:45 -0400

    allowed build script to handle pubsub trigger func

--- build/build-only-changed-gcp-functions.sh
+++ build/build-only-changed-gcp-functions.sh
@@ -9,11 +9,18 @@ echo $ALL_DIRS
 
 for f in ${CHANGED_FILES}; do
    fname=`echo $f | awk -F/ '{print $2}'`
-   # echo $f
+   # if one of the GCP Functions changed then perform a deploy on that specific function
    if [[ $ALL_DIRS == *"$fname"* ]]; then 
       echo $fname; 
-      #gcloud --region="us-central1" --quiet beta functions deploy $fname --source https://source.developers.google.com/projects/scenic-setup-231121/repos/github_fhafez_signin-template-node/moveable-aliases/master/paths/gcp-functions/$fname --trigger-http 
-      gcloud --quiet beta functions deploy $fname --source https://source.developers.google.com/projects/scenic-setup-231121/repos/github_fhafez_signin-template-node/moveable-aliases/master/paths/gcp-functions/$fname --trigger-http 
+
+      # if this function is triggered by PubSub then it's name must end in .sub
+      if [[ $fname == *".sub"]]; then
+         topic=`echo $f |  cut -d'_' -f 2 | rev | cut -c5- | rev`;
+         gcloud --quiet beta functions deploy $fname --source https://source.developers.google.com/projects/scenic-setup-231121/repos/github_fhafez_signin-template-node/moveable-aliases/master/paths/gcp-functions/$fname --trigger-topic $topic --runtime nodejs8 --memory=128MB
+      else
+         #gcloud --region="us-central1" --quiet beta functions deploy $fname --source https://source.developers.google.com/projects/scenic-setup-231121/repos/github_fhafez_signin-template-node/moveable-aliases/master/paths/gcp-functions/$fname --trigger-http 
+         gcloud --quiet beta functions deploy $fname --source https://source.developers.google.com/projects/scenic-setup-231121/repos/github_fhafez_signin-template-node/moveable-aliases/master/paths/gcp-functions/$fname --trigger-http 
+      fi;
    fi;
 done
 
