7452489c3b3caf9816ae50656fb7860e968784e3
fhafez <fadi.hafez@gmail.com>
2020-05-13 21:47:27 -0400

    another try

--- gcp-functions/function-scar_appointment.sub/index.js
+++ gcp-functions/function-scar_appointment.sub/index.js
@@ -7,9 +7,9 @@ exports.scar_appointment = async (data, context) => {
     const msg = data;
     const details = msg.data ? Buffer.from(msg.data, 'base64').toString() : '';
 
-    let services = JSON.parse(details);
+    let services = JSON.parse(details).services;
 
     // just print the message received - that's all
     console.log(`Got a message! ${details}`);
-    console.log(`Services: ${services}`);
+    console.log(`Services: ${JSON.stringify(services)}`);
 };
