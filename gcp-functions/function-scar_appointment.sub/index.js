b6d7a5c10bc14031b70be4c3a9a60961166ac491
fhafez <fadi.hafez@gmail.com>
2020-05-13 21:39:26 -0400

    printing services too

--- gcp-functions/function-scar_appointment.sub/index.js
+++ gcp-functions/function-scar_appointment.sub/index.js
@@ -7,6 +7,9 @@ exports.scar_appointment = async (data, context) => {
     const msg = data;
     const details = msg.data ? Buffer.from(msg.data, 'base64').toString() : '';
 
+    let services = JSON.parse(details);
+
     // just print the message received - that's all
     console.log(`Got a message! ${details}`);
+    console.log(`Services: ${services}`);
 };
