daca521290744913e32657703ece46265d78d0c7
fhafez <fadi.hafez@gmail.com>
2020-05-14 01:06:54 -0400

    now writing to datastore

--- gcp-functions/function-scar_appointment.sub/index.js
+++ gcp-functions/function-scar_appointment.sub/index.js
@@ -1,15 +1,95 @@
 const {PubSub} = require('@google-cloud/pubsub');
+const atob = require('atob');
+const {Datastore} = require('@google-cloud/datastore');
+const datastore = new Datastore({
+	projectId: 'scenic-setup-231121'
+});
+
+const {Storage} = require('@google-cloud/storage');
+const storage = new Storage();
+const BUCKET_NAME = "scarsigs.parcsignin.com";
+
+const kindName = 'Appointment';
 
 const pubSubClient = new PubSub();
 
 exports.scar_appointment = async (data, context) => {
-  
+
     const msg = data;
     const details = msg.data ? Buffer.from(msg.data, 'base64').toString() : '';
+    let params = JSON.parse(details);
 
-    let services = JSON.parse(details).services;
+    const unixTimestamp = new Date().getTime() * 1000;
+    let appointmentID = params.apptID || 0;
+    let patientID = params.patientID || '';
+    let firstname = params.firstname || '';
+    let lastname = params.lastname || '';
+    let dob = params.dob || '';
+    let current_datetime = params.current_datetime;    
+    let signedInAt = params.signedInAt || current_datetime;
+    let signedOutAt = params.signedOutAt || 0;
+    let signature = params.sig || '';
+    let signatureFilename = firstname + '_' + lastname + '_' + unixTimestamp + '.svg';
+    let services = params.services || [];
+    let staff = params.staff || '';
 
-    // just print the message received - that's all
-    console.log(`Got a message! ${details}`);
-    console.log(`Services: ${JSON.stringify(services)}`);
+    // Upload a new file to Cloud Storage if we have events to save
+    if (signature.length) {
+        const file = storage.bucket(BUCKET_NAME).file(signatureFilename.trim());
+        console.log(`Saving signature to ${signatureFilename} in bucket ${BUCKET_NAME}`);
+
+        file.save(atob(signature), {'metadata':{contentType: 'image/svg+xml'}}).then(() => {
+            console.log(`JSON written to ${signatureFilename.trim()}`);
+        })
+        .catch(err => {
+            console.error('ERROR:', err);
+            return;
+        });;
+    }
+        
+    // first update the patient services counters, and set the signed_in to true
+    const patientKey = datastore.key({path: ['Patient', datastore.int(patientID)]});
+    var dataToSave = {
+        firstname: firstname,
+        lastname: lastname,
+        dob: dob,
+        signed_in: 1,
+        services: services
+    }
+            
+    datastore.save({key: patientKey, data: dataToSave})
+        .catch(err => {
+            console.error('ERROR:', err);
+            return;
+        });
+            
+    // then create the appointment
+    dataToSave = {
+        patientID: patientID,
+        firstname: firstname.trim(),
+        lastname: lastname.trim(),
+        dob: dob.trim(),
+        signatureFilename: signatureFilename.trim(),
+        signedInAt: datastore.int(signedInAt),
+        signedOutAt: datastore.int(signedOutAt),
+        staff: staff
+        //				time_create: datastore.int(Math.floor(new Date().getTime()/1000))
+    }
+
+    const key = appointmentID > 0 ? datastore.key({path: ['Appointment', datastore.int(appointmentID)]}) : datastore.key(kindName);
+    
+  	console.log('saving data ' + key.path);  
+    console.log("now going to datastore");
+	datastore
+		.save({
+			key: key,
+            data: dataToSave,
+		})
+		.catch(err => {
+		    console.error('ERROR:', err);
+		    return;
+		});
+
+    console.log("finished the datastore");
+
 };
