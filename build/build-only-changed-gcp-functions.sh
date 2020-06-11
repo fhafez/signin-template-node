8f1e4067c43056397db411dc66198f301081980a
Fadi Hafez <fadihafez@personal-macbook.lan>
2020-06-11 11:56:11 -0400

    updated build script

--- build/build-only-changed-gcp-functions.sh
+++ build/build-only-changed-gcp-functions.sh
@@ -9,13 +9,7 @@ echo $CHANGED_FILES
 echo $ALL_DIRS
 
 for f in ${CHANGED_FILES}; do
-   fname=`echo $f | awk -F/ '{print $2}'`
-
-   # if the file was .html or .js then update it in GCS
-   if [[ $f == *".js" || $f == *".html" ]]; then
-      gsutil cp /home/travis/build/fhafez/signin-template-node/$f gs://index.parcsignin.com/whitby/$f
-   fi;
-   
+   fname=`echo $f | awk -F/ '{print $2}'`   
 
    # if one of the GCP Functions changed then perform a deploy on that specific function
    if [[ $ALL_DIRS == *"$fname"* && $COMPLETED != *"$fname"* ]]; then 
@@ -25,10 +19,15 @@ for f in ${CHANGED_FILES}; do
       # if this function is triggered by PubSub then it's name must end in .sub
       if [[ $fname == *".sub" ]]; then
          topic=`echo $fname |  cut -d'-' -f 2 | rev | cut -c5- | rev`;
-         gcloud --quiet beta functions deploy $topic --source https://source.developers.google.com/projects/signaturemountain-240415/repos/github_fhafez_signin-template-node/moveable-aliases/signaturemountain-240415/paths/gcp-functions/$fname --trigger-topic $topic --runtime nodejs8 --memory=128MB
+         gcloud --quiet beta functions deploy $topic --source https://source.developers.google.com/projects/signaturemountain-240415/repos/github_fhafez_signin-template-node/moveable-aliases/signaturemountain-240415/paths/gcp-functions/$fname --trigger-topic $topic --runtime nodejs10 --memory=128MB
       else
          #gcloud --region="us-central1" --quiet beta functions deploy $fname --source https://source.developers.google.com/projects/scenic-setup-231121/repos/github_fhafez_signin-template-node/moveable-aliases/master/paths/gcp-functions/$fname --trigger-http 
-         gcloud --quiet beta functions deploy $fname --source https://source.developers.google.com/projects/signaturemountain-240415/repos/github_fhafez_signin-template-node/moveable-aliases/signaturemountain-240415/paths/gcp-functions/$fname --trigger-http 
+         gcloud --quiet beta functions deploy $fname --source https://source.developers.google.com/projects/signaturemountain-240415/repos/github_fhafez_signin-template-node/moveable-aliases/signaturemountain-240415/paths/gcp-functions/$fname --trigger-http --runtime nodejs10 --memory 128M 
+      fi;
+   else
+      # if the file was .html or .js then update it in GCS
+      if [[ $f == *".js" || $f == *".html" ]]; then
+         gsutil cp /home/travis/build/fhafez/signin-template-node/$f gs://index.parcsignin.com/whitby/$f
       fi;
    fi;
 done
